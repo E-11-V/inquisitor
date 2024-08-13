559353d0d34e5d2b94f91c8ffdc61873b3059347
E <84926127+E-11-V@users.noreply.github.com>
2024-08-14 01:06:38 +0200

    Update inquisitor.py

--- inquisitor.py
+++ inquisitor.py
@@ -3,10 +3,100 @@ from PyPDF2 import PdfReader
 from colorama import init, Fore, Style
 import sys
 import signal
+import logging
+import time
+from PyPDF2 import PdfReader
+
+
+
+
+# Disable PyPDF2 debug logging
+logger = logging.getLogger('PyPDF2')
+logger.setLevel(logging.WARNING)
 
 # Initialize colorama
 init()
+# Display the visual pattern
+visual_pattern = """
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+...........................................:---::::......::::.......................................
+......................................:==---::.................:::..................................
+....................................-*+=--::....................::::::..............................
+..................................:**=---::::..............:...::::::--:............................
+..............................::-=++=---=:::::.....::.::::.......:..:::-+:..........................
+............................-+*+*++===--===+*===:==-:::-=:--::::::::::::-=-.........................
+.........................:=+###%@@%##%%@%@@@%@@@%%@#*+-==-:::::---=--::--=+-.:......................
+.......................:+##*%@@@@%%%@%@%*+***%++%@%+@@@@*#*+=-::---===:--=+%#*==....................
+.......................=#*#@%@@%@%#+-----::::::-:-:.:-++::-**+=--==-==--:-++***+=...................
+......................-#%##@@@*##+=-::::::-:--::::..........-##%#*++===*==+==*%#++:.................
+......................=*%%@@%%##*+---:::--=-+-:::.::.......:::-+%+%%%*+===++==#%%+++................
+......................-*%%%##++++=--:::::-+=+-:::::.........::-:::-%%%##-===+@%%@%#*:...............
+.......................-+*+#**=++*=-::::-:--*-:::::..........:::::::=##+#*+-=#%#%%%#:...............
+..........................:---+--++*+=-=+-++#=+=:.:::::::...:::-:-::+%#@##*=*%%%%@**=-..............
+..............................*#%#%%@@%+=---%%#%%@@*==+-==+::::::-::-:+@@---%%@%##*+:...............
+..............................+@*%@%@@@#===-+#*#@@@@%=-:::.=-:...::::-==.=*++@@@%**-................
+...............................*#@@%@@%@@==-=+*@%@@%#==*=-:-:......::::::=::-##%##-:................
+...............................-%#@#-##%%*-:==+=+#%+-==*+=:....::.::::::--=:-*###*+-................
+...............................:@%=--*#@+:...-+*:::.........:....::::::-*+-:#%%*+=:.................
+...............................++*---+#%:.....:=+-:..............::::::=#.=--:--....................
+..............................:*==-**=**::.....:-==:...........:.:::::::.::.........................
+..............................+*+--::-++::..::..:::-:........:::::::::-+-::::............:...::.....
+..............................=#+:::-+#*-:..:.::::.::........:::::.::::=::::.:......::::..:::::.....
+...............................*+-:-+*.-:...:...--...........:::....:.:=::.................:........
+...............................:%+=*%%---...::::+*-:........:::....::::+%+::::::::.:::::::..........
+..................:........::..:-###++#%++=+#@#%++-::......::-:::.::::=+:-*=+-::::::::::.:::...::...
+...............:...............::#**====*%##:..::---::.....:::....:::-*::%@++=#*-::.:::::.:::...:...
+................................:**++*-=+::=..:::-:::::.:::-:.....:-=+::*@@@**=*#::.::::..::...::...
+.........::::..........:........:*+-*@*+*#+*+==-#@=:.::::--:::::::-=*:.*@@@#++-+#+:::::::::::..::::.
+.........:::.....:...........::::=+-=-==+++*#+-.:-==:.::----:::::-=-..*@@@+:-+=-#*-:::::.:::.:::::::
+.................................-*+==::-::-----:::..-:::-+-----*-..:#@@@-=--*=:=**:::::::::.:::::::
+..................:........:::::*@@#*+=+-:::::.:::::::-::-+++-=+:..+%%@+:----#-:=##*-:::::::::::::::
+..::.......:::...::...:.:::.:-*@@@@@%*##-+=-::::.:--==+=*=-=**-.:#%#@@-:-=--=@--*+*%#-::::::::::::::
+....:......:....:::...:::::-#@@@@@@@%=*+=+*+==---=+++*%@#*+:..=%##@@+:-----=%=:=***#%+::::::::::::::
+......................:-+#@@@@%%@@@%@*::=+*####+#%#+-:...:.-#@%**%+::---:=#:::===+**#%-:::::::::.:::
+........:::....:.:::-#@@@@@@@@@@%@%#%@@%*=:::#@@@#::.:-+*%@@%*%*=:::---=+::::--+*#*#*@#-::::::::::::
+.:.:::.::.::.::.::*@@@@@@@@@@@%%@%%%*+#@@@@@#@@@@@@@@@@@@%#%+::::-----=::----:--+*#@@@@@%*-:::::::::
+..::.:::::::..:=%@@@@@@@@@@@@@@@%%##*+*+=*@@@@@@@@@@@@@*-::--:::-----::--::::::-*@@%%@@@@@@%=:::::..
+:.:.::::::=#@@@@@%@@@@#%@@@@@@@@@###=*#=*#=::--=%@@+.-=--::::::--+-:::---::-:-#@@#####%%#@%%@@#::.::
+::::::=#@@@@@@@@@@@@@%%%%%@%@@%%@@@%##++#+++=--:#@%::---------=#-.:-----:---%@@@%##**##%%##%#@%@%-:.
+:::-+@@@@@@@@%%@@@%@%#%%@@%@@%@@%%@@@@##*-=#+==-+@=-*#+++=###=:::-::-----+%@%#%#*#**#*#****#%%###%@+
+:-+%@@@%%%%%@#%@%@@#%%%%%%@@@@@@#@%@#%@%%%#+=@#-=@-#+::...:..:::::::---+%@@%*++*=++++++=+===#**=+###
++@@%%%%%%%%#*%#*##***##%#%#%%#%%@@%%@%%@%@@@@#+@*%*-=-:::::--:::::::=%*##%#-:-==+=+*=====--+++=++#*+
+@#%%%%%%@###**#%*#%####**##%#%%%%@@%#@#@@%@@@@@@@#---::::-::--==+#=+*+%#%#+-====+=++==-+=+++*==+**+*
+###%#%%%%%%+#+%##**+**+**####%%%#%@%%%#%%%@%%%@@@%::::::-:--*+=-----+#*--=--===-+#+*+==+======++**#+
+*+#*++#***+*+*+#+#+++#**##%######%@@%**##%%%%%@@@%--::::=##==------+%*=---=:=+-+===-:--+===--=+++-=+
+**#**==-===+=++=+=**##**+#***#*#*#**%**#####%%@%%%---*#=------:--##%------====+-+=-=-==-=--=-===-=++
+*+++=+-+-=-+=-=-=--=+=++++#*%***#++:*%%*+*%#%%%%%%--#=---:--:---=##--:---:-----::------==----=----+-
+-----::::::::::::::---=====-=+++++=--%%+=%##%#%#%+:=-::::::::-***::.:..:::::::::::::::-:-:---:::----
+:::::::::::::.....::----===-===+#+=-::##+**%%%%@#=---:::::::-*+-...............::.:..::::::-:--:----
+:............:.....:::::::=-====***=--=@#**#%%@=*#=-:--:-:-#**:.........::::..:::......::..:::------
+....................:::::::--:-+*+*#=--=%#=#%#::-=:::-:::=#+-:................................::::=:
+..................:.......:::----+++#-:-*%##@--:::::::::%%#:::......................................
+....................:....:::::=+++#***+-=-%#-%#+-::::+%@=:::::.:....................................
+.....................::..::::::==+**##*#==+%#=*--::-#%%=::::........................................
+.......................:..::::::==*#+*#*+=*#+#=-:-##@-:::.:.........................................
+........................::.::::::=+#++*#%#@-:=%%#%*-:...:...........................................
+.........................-.::::::-+#*#*#%%%:+-==#-:-::..:::::::.....................................
+..........................:.:::::-==#*##%%%=%*=-%-*=:.....::::......................................
+...........................:..::::--*+#%#%@=----#--:................................................
+............................::.::::--==##%@*=---+=---:::.::.........................................
+..............................:::::-=-=+#%%%+#==**-===-:::..........................................
+.................................:--===+*#%@=:-:*#+-.:.:............................................
+..................................-=-=+-*+#%-:::#+=-................................................
+..................................:--==---*#-:--%+--::..............................................
+...................................:=:-=-==#+:::%===:...............................................
+...................................::-:-=--#=.::....................................................
+"""
 
+
+print(visual_pattern)
 # Paths for the output files
 output_ocred_file = 'output_ocred.txt'
 output_image_file = 'output_image.txt'
@@ -21,6 +111,8 @@ def signal_handler(sig, frame):
     stop_processing = True
     print(f"\n{Fore.YELLOW}Processing stopped by user. Exiting...{Style.RESET_ALL}")
     sys.exit(0)
+print(f"{Fore.CYAN}The Inquisitor is reading...{Style.RESET_ALL}")
+time.sleep(5)
 
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -50,22 +142,16 @@ def page_has_text(pdf, page_number):
 
 # Process a single PDF file
 def process_pdf(pdf_path, processed_ocred, processed_image, processed_doublecheck):
-    status_message = "Unknown status"  # Initialize with a default value
-
-    # Skip if already processed
     if pdf_path in processed_ocred or pdf_path in processed_image or pdf_path in processed_doublecheck:
-        return None  # Indicate no status change
+        return  # Skip if already processed
 
     try:
-        # Check if the PDF file exists
         if not os.path.isfile(pdf_path):
-            print(f"{Fore.YELLOW}File not found: {pdf_path}{Style.RESET_ALL}")
-            return None  # Indicate no status change
+            return
 
         pdf = PdfReader(pdf_path)
         num_pages = len(pdf.pages)
 
-        # Determine pages to check
         pages_to_check = []
         if num_pages >= 36:
             pages_to_check = [8, 16, 34]  # 0-based index
@@ -76,37 +162,29 @@ def process_pdf(pdf_path, processed_ocred, processed_image, processed_doublechec
         else:
             pages_to_check = [0, num_pages - 1]
 
-        # Check the pages
-        text_found = False
-        no_text_pages = 0
         has_text_pages = 0
+        no_text_pages = 0
 
         for page in pages_to_check:
             if stop_processing:
-                return None  # Stop processing if a stop signal is received
+                return
             if page_has_text(pdf, page):
                 has_text_pages += 1
-                text_found = True
             else:
                 no_text_pages += 1
 
-        # Classification based on pages with text
+        # Classification and file writing are handled, but without detailed status messages
         if has_text_pages > 0 and no_text_pages == 0:
             write_to_output_file(output_ocred_file, pdf_path)
-            status_message = "File with OCR text"
         elif has_text_pages > 0 and no_text_pages > 0:
             write_to_output_file(output_doublecheck_file, pdf_path)
-            status_message = "File with some OCR text (needs double-check)"
         else:
             write_to_output_file(output_image_file, pdf_path)
-            status_message = "File without OCR text"
 
     except IOError as e:
         print(f"{Fore.RED}IOError processing {pdf_path}: {e}{Style.RESET_ALL}")
     except Exception as e:
         print(f"{Fore.RED}Error processing {pdf_path}: {e}{Style.RESET_ALL}")
-
-    return status_message
 
 # Recursively process directories to find PDF files
 def process_directories(root_dir, processed_ocred, processed_image, processed_doublecheck):
@@ -124,31 +202,26 @@ def process_directories(root_dir, processed_ocred, processed_image, processed_do
 
     for i, pdf_path in enumerate(pdf_files_list):
         if stop_processing:
-            return  # Stop processing if a stop signal is received
-        status_message = process_pdf(pdf_path, processed_ocred, processed_image, processed_doublecheck)
-        if status_message:
-            processed_files += 1
+            return
+        process_pdf(pdf_path, processed_ocred, processed_image, processed_doublecheck)
+        processed_files += 1
 
         # Dynamic progress bar update
         progress = (i + 1) / total_files * 100
-        sys.stdout.write(f"\r{Fore.GREEN}Processing: {processed_files}/{total_files} files ({progress:.2f}% complete) - {status_message}{Style.RESET_ALL}")
+        sys.stdout.write(f"\r{Fore.GREEN}Processing: {processed_files}/{total_files} files ({progress:.2f}% complete){Style.RESET_ALL}")
         sys.stdout.flush()
 
+    # Summary is optional; remove or simplify if needed
     print(f"\n{Fore.MAGENTA}Summary for {root_dir}:{Style.RESET_ALL}")
     print(f"{Fore.CYAN}Total files found: {total_files}{Style.RESET_ALL}")
     print(f"{Fore.CYAN}Files processed: {processed_files}{Style.RESET_ALL}")
-    print(f"{Fore.GREEN}Files with OCR text: {len(load_processed_files(output_ocred_file))}{Style.RESET_ALL}")
-    print(f"{Fore.CYAN}Files needing double-check: {len(load_processed_files(output_doublecheck_file))}{Style.RESET_ALL}")
-    print(f"{Fore.RED}Files without OCR text: {len(load_processed_files(output_image_file))}{Style.RESET_ALL}")
 
 # Main function to process the PDFs
 def process_pdfs(paths_file):
-    # Load already processed files
     processed_ocred = load_processed_files(output_ocred_file)
     processed_image = load_processed_files(output_image_file)
     processed_doublecheck = load_processed_files(output_doublecheck_file)
 
-    # Read the paths from paths.txt
     try:
         with open(paths_file, 'r', encoding='utf-8') as f:
             folder_paths = f.read().splitlines()
@@ -161,7 +234,6 @@ def process_pdfs(paths_file):
             print(f"{Fore.RED}Directory not found: {folder_path}{Style.RESET_ALL}")
             continue
         
-        # Process the directory and its subdirectories
         print(f"{Fore.YELLOW}Processing directory: {folder_path}{Style.RESET_ALL}")
         process_directories(folder_path, processed_ocred, processed_image, processed_doublecheck)
 
